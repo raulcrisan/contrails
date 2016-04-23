c3d821a731512770632d7bf4ad2d36543c642b4b
Rares Barbantan <rares.barbantan@isdc.eu>
2016-04-23 15:46:35 +0300

    quickfix: exclusion list: nexus5x and nexus6p

--- android/app/src/main/java/ro/spaceapps/contrails/ImageProcessor.java
+++ android/app/src/main/java/ro/spaceapps/contrails/ImageProcessor.java
@@ -1,6 +1,8 @@
 package ro.spaceapps.contrails;
 
 import android.graphics.SurfaceTexture;
+import android.media.Image;
+import android.util.Log;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
@@ -11,10 +13,14 @@ import org.opencv.core.Mat;
  * Created by rares.barbantan on 23/04/16.
  */
 public class ImageProcessor implements GuidedPhotoPreview.FrameCallback {
+    private static final String TAG = ImageProcessor.class.getName();
     boolean shouldFlip = false;
     @Override
     public void onCameraViewStarted(SurfaceTexture texture, int width, int height) {
-
+        //shame on me and android for doing this
+        Log.d(TAG, "device: " + android.os.Build.DEVICE);
+        shouldFlip = android.os.Build.DEVICE.equals("bullhead")
+                || android.os.Build.DEVICE.equals("shamu");
     }
 
     @Override
